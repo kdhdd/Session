8557364667cdfebcd7b3b91daf261b65ab255d8b
kdhdd <109668066+kdhdd@users.noreply.github.com>
2024-05-12 23:53:21 +0900

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,67 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "age")
+    private int age;
+
+    public Employee(){}
+
+    public Employee(int id, String name, String department, int age) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
